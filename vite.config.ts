03db22eabbfa097444d59062f5e9ded6b55c4592
Bardan Neupane <nbardan10@gmail.com>
2025-05-07 12:52:12 +0545

    Added deploy steps for github account in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/testsite/', // Replace 'repository-name' with your repo's name
+  base: '/testproject/', // Replace 'repository-name' with your repo's name
   build: {
     outDir: 'dist',  // The folder Vite will use to store the built files
   },
